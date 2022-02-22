4ae0f7b90f131e47f6e37d589749f0abeaec51a4
anonymous anonymous <ashlyn.adelman@kellogg.northwestern.edu>
2022-02-22 20:51:19 +0000

    Direct association between categories and items

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Item, type: :model do
   
     describe "Direct Associations" do
 
+    it { should belong_to(:category) }
+
     it { should belong_to(:price) }
 
     it { should have_many(:searches) }
